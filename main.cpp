d20bec9c2f7c25ad8de3b67eb9c7b6f21a57c7b9
jocoso <joshua.collado022@gmail.com>
2023-05-09 09:21:47 -0400

    eliminated Hud class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <SFML/Graphics.hpp>
 #include "Engine/engine.h"
-#include "Engine/Hub.h"
 #include "Engine/aspect.h"
 #include <windows.h>
 
@@ -12,14 +11,6 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
     sf::RenderWindow window(sf::VideoMode(800, 500), "prototype 0.0.0");
     Engine e(&window);
-
-    Hub hub;
-    Aspect *as = &hub;
-
-    s0.register_prop("main_hub");
-    s0.add_aspect("main_hub", as);
-
-    e.add_scene(&s0);
 
     e.run();
     return 0;
