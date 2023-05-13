1831e45ff29cd86e89cb994666c489bc071f1f90
jocoso <joshua.collado022@gmail.com>
2023-05-13 09:49:17 -0400

    Changed main file

--- main.cpp
+++ main.cpp
@@ -1,26 +1,15 @@
 #include <SFML/Graphics.hpp>
 #include "Engine/engine.hpp"
 #include "Engine/imagener.hpp"
-#include <windows.h>
 
-INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    PSTR lpCmdLine, INT nCmdShow) {
+int main() {
     Scene s0;
 
     sf::RenderWindow window(sf::VideoMode(800, 500), "prototype 0.0.0");
     Engine e(&window);
 
-    Imagener img("C:\\Users\\Jocoso2218\\Desktop\\cooloutfits.jpg");
-    img.changeSize(400);
-
-    Aspect *as = &img;
-
-    s0.register_prop("floor");
-    s0.add_aspect("floor", as);
-
     int i = e.add_scene(&s0);
     
-
     e.run();
     return 0;
 }
